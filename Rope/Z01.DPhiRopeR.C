329d86a216d048fd77b9d2a484ae448a9bf87e68
Dong Jo Kim <dong.jo.kim@cern.ch>
2019-04-18 23:28:27 +0300

    new files

--- Rope/Z01.DPhiRopeR.C
+++ Rope/Z01.DPhiRopeR.C
@@ -1,11 +1,8 @@
 
-#include "include/Filipad.h"
+void setStyle();
 
 void LoadData();
 void compare();
-void DrawSignal(int padid, int iPTT, int iPTA);
-void DrawPP(int padID, int iPTT, int iPTA);
-void DrawIAA(int padID, int iPTT, int iPTA);
 
 double lowx=-1.6;
 double highx=3.1;
@@ -15,9 +12,10 @@ double hy = 0.2;
 TLatex latexRun;
 TString strRun = "pp #sqrt{#it{s}} = 13 TeV";
 
-const int Nsets = 1;
+const int Nsets = 2;
 TString infiles[Nsets] = {
-	"pythia8240_pp13TeV_Rope_1-2GeV.root"
+	"pythia8240_pp13TeV_RopegAmplitude10.0_1-2GeV.root",
+	"pythia8240_pp13TeV_RopegAmplitude10.0_2-3GeV.root"
 };
 TFile *fin[Nsets];
 
@@ -45,9 +43,11 @@ double ptmin=1.0;
 double ptmax=2.0;
 
 int iRef=0;
-
+TCanvas *canvas[NMultBins];
+TCanvas *c1;
 void Compare();
 void DrawDPhi(int padID);
+void DrawDPhiAll(int iE);
 
 //------------------------------------------------------------------------------------------------
 void LoadData() {
@@ -88,59 +88,92 @@ void Compare(){
 	LoadData();
 
 	int ic=0;
-	for(int ic=0; ic<NMultBins; ic++){
-			DrawDPhi(ic++);
-	}
+	//DrawDPhiAll(kCMS);
+	DrawDPhiAll(kALICE);
+	//for(int ic=0; ic<NMultBins; ic++){
+	//		DrawDPhi(ic++);
+	//}
 
 }
 
 
 //------------------------------------------------------------------------------------------------
 void DrawDPhi(int padID) {
-	Filipad *fpad[NMultBins];
 	for(int ic=0;ic<NMultBins;ic++) {
-		fpad[ic] = new Filipad(padID+ic+1, 1.1, 0.3, 100, 100, 0.7,NMultBins);
-		fpad[ic]->Draw();
-		//==== Upper pad
-		TPad *p = fpad[ic]->GetPad(1); //upper pad
-		p->SetTickx(); p->SetLogx(0); p->SetLogy(0); p->cd();
-		hy = hDeltaPhi[0][kCMS][ic]->GetMaximum()*1.02;
-		//ly = hDeltaPhi[0][kCMS][ic]->GetMinimum()*0.02;
-		TH2F *hfr = new TH2F("hfr"," ", 100,lowx, highx, 10, ly, hy); // numbers: tics x, low limit x, upper limit x, tics y, low limit y, upper limit y
-		hset( *hfr, "#Delta#phi", "1/N_{trigg} dN/d#Delta#phi",1.1,1.0, 0.09,0.09, 0.01,0.01, 0.04,0.05, 510,505);//settings of the upper pad: x-axis, y-axis
-		hfr->Draw();
 		//Legend definition
+		canvas[ic] = new TCanvas(Form("c%d",ic+1), "Canvas");
+		gStyle->SetOptStat(0);gStyle->SetOptTitle(0);
+		//setStyle();
 		TLegend *leg = new TLegend(0.35,0.6,0.85,0.85,"","brNDC");
 		leg->SetTextSize(0.037);leg->SetBorderSize(0);leg->SetFillStyle(0);//legend settings;
-
-		latexRun.DrawLatexNDC( 0.4, 0.9 ,strRun);
 		leg->AddEntry((TObject*)NULL,sLeg[0],"");
 		leg->AddEntry((TObject*)NULL,hDeltaPhi[0][kALICE][ic]->GetTitle(),"");
-
-		for(int iS=0;iS<Nsets;iS++) {
-			for(int i = 0; i<Nexp; i++) {
-				hDeltaPhi[iS][i][ic]->SetMarkerStyle(gMarkers[i]);
-				hDeltaPhi[iS][i][ic]->SetMarkerColor(gColors[i]);
-				hDeltaPhi[iS][i][ic]->SetLineColor(gColors[i]);
-				hDeltaPhi[iS][i][ic]->Draw("p,same");
-				leg->AddEntry(hDeltaPhi[iS][i][ic],expName[i],"pl");
-			}
+		for(int i = 0; i<Nexp; i++) {
+				hDeltaPhi[0][i][ic]->SetMarkerStyle(gMarkers[i]);
+				hDeltaPhi[0][i][ic]->SetMarkerColor(gColors[i]);
+				hDeltaPhi[0][i][ic]->SetLineColor(gColors[i]);
+				hDeltaPhi[0][i][ic]->Draw("p,same");
+				leg->AddEntry(hDeltaPhi[0][i][ic],expName[i],"pl");
 		}
+		hDeltaPhi[0][kALICE][ic]->GetXaxis()->SetTitle("#Delta#phi");
+		hDeltaPhi[0][kALICE][ic]->GetXaxis()->CenterTitle();
+		auto rp = new TRatioPlot(hDeltaPhi[0][kALICE][ic], hDeltaPhi[0][kCMS][ic]);
+		//canvas[ic]->SetTicks(0, 1);
+   		rp->Draw();
+   		
+   		rp->GetLowYaxis()->SetNdivisions(505);
+   		rp->GetLowerRefXaxis()->SetTitle("#Delta#phi");
+   		rp->GetLowerRefYaxis()->SetTitle("ALICE/CMS");
+   		rp->GetUpperRefYaxis()->SetTitle("1/N_{trigg} dN/d#Delta#phi");
+   		rp->GetUpperRefXaxis()->CenterTitle();
+   		rp->GetUpperRefYaxis()->CenterTitle();
+   		rp->GetUpperPad()->cd(); 
+   		latexRun.DrawLatexNDC( 0.4, 0.9 ,strRun);
+   		leg->Draw();
+   		canvas[ic]->Update();
+   	}
+}
 
+//------------------------------------------------------------------------------------------------
+void DrawDPhiAll(int iE) {
+	c1 = new TCanvas("c1", "Canvas");
+	gStyle->SetOptStat(0);gStyle->SetOptTitle(0);
+	TLegend *leg = new TLegend(0.35,0.6,0.85,0.85,"","brNDC");
+	leg->SetTextSize(0.037);leg->SetBorderSize(0);leg->SetFillStyle(0);//legend settings;
+	leg->AddEntry((TObject*)NULL,sLeg[0],"");
+	//leg->AddEntry((TObject*)NULL,hDeltaPhi[0][kALICE][0]->GetTitle(),"");
+	for(int ic=0;ic<NMultBins;ic++) {
+		hDeltaPhi[0][iE][ic]->SetMarkerStyle(gMarkers[ic]);
+		hDeltaPhi[0][iE][ic]->SetMarkerColor(gColors[ic]);
+		hDeltaPhi[0][iE][ic]->SetLineColor(gColors[ic]);
+		hDeltaPhi[0][iE][ic]->GetXaxis()->SetTitle("#Delta#phi");
+		hDeltaPhi[0][iE][ic]->GetXaxis()->CenterTitle();
+		hDeltaPhi[0][iE][ic]->GetYaxis()->CenterTitle();
+		hDeltaPhi[0][iE][ic]->GetYaxis()->SetTitle("1/N_{trigg} dN/d#Delta#phi");
+		hDeltaPhi[0][iE][ic]->Draw("p,same");
+		leg->AddEntry(hDeltaPhi[0][iE][ic],hDeltaPhi[0][iE][ic]->GetTitle(),"pl");
+   	}
+   		//rp->GetUpperPad()->cd(); 
+   	latexRun.DrawLatexNDC( 0.4, 0.9 ,strRun);
+   	leg->Draw();
+   	c1->Update();
 
-		leg->Draw();
+}
 
-		//==== Lower pad
-		p = fpad[ic]->GetPad(2);
-		p->SetTickx(); p->SetGridy(1); p->SetLogx(0), p->SetLogy(0); p->cd();
-		TH2F *hfr1 = new TH2F("hfr1"," ", 100, lowx, highx, 10, 0.95, 1.05);
-		hset( *hfr1, "#Delta#phi", "ALICE/CMS",1.1,1.0, 0.09,0.09, 0.01,0.01, 0.04,0.05, 510,505);
-		hfr1->Draw();
-		for(int iS=0;iS<Nsets;iS++){
-			hDeltaPhi_Ratio[iS][kALICE][ic]->SetMarkerStyle(20);
-			hDeltaPhi_Ratio[iS][kALICE][ic]->Draw("psame");
-		}
-		
-		gPad->GetCanvas()->SaveAs(Form("figs/DeltaPhi_C%02d.pdf",ic));
-	}
+void setStyle(){
+        gStyle->SetTitleFont(22,"y");
+        gStyle->SetTitleFont(22,"z");
+        gStyle->SetLabelFont(22,"x");
+        gStyle->SetLabelFont(22,"y");
+        gStyle->SetLabelFont(22,"z");
+        gStyle->SetTitleSize(0.06,"x");
+        gStyle->SetTitleSize(0.06,"y");
+        gStyle->SetLabelSize(0.05,"x");
+        gStyle->SetLabelSize(0.05,"y");
+        gStyle->SetTitleOffset(0.9,"x");
+        gStyle->SetTitleOffset(0.8,"y");
+        gStyle->SetNdivisions(505);
+        gStyle->SetOptStat(0);
+        // gStyle->SetOptTitle(0);
+        gStyle->SetOptFit(1);
 }
